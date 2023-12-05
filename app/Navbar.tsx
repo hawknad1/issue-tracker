2c3774d3c3913db0aa1253a17196a797f294b74e
Thomas Dankwah <dankwahthom@gmail.com>
2023-12-05 02:57:41 +0000

    Style the active link in the navbar

---
+++ app/Navbar.tsx
@@ -0,0 +1,34 @@
+"use client";
+
+import classNames from "classnames";
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+import React from "react";
+
+const Navbar = () => {
+  const pathname = usePathname();
+  const navLinks = [
+    { label: "Dashboard", href: "/" },
+    { label: "Issues", href: "/issues" },
+  ];
+  return (
+    <nav className="flex space-x-6 border-b mb-5 px-5 h-14 items-center">
+      <Link href="/">Logo</Link>
+      {navLinks.map((link) => (
+        <Link
+          key={link.href}
+          className={classNames({
+            "text-zinc-950": pathname === link.href,
+            "text-zinc-500": pathname !== link.href,
+            "hover:text-zinc-800 transition-colors": true,
+          })}
+          href={link.href}
+        >
+          {link.label}
+        </Link>
+      ))}
+    </nav>
+  );
+};
+
+export default Navbar;
